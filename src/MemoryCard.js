baeb2812096e444348055558bed52f707a5e7e26
Melissa Cantu <meli_cantu12@hotmail.com>
2018-09-10 13:53:45 -0500

    first project memory game

---
+++ src/MemoryCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './MemoryCard.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="MemoryCard">
+
+                <img src={"https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"}/>
+            </div>
+
+        );
+    }
+}
+
+export default App;
