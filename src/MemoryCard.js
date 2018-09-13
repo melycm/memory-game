a99d47699af8de1b423007413ad3945ed376b806
Melissa Cantu <meli_cantu12@hotmail.com>
2018-09-12 21:58:12 -0500

    part 2

--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import './MemoryCard.css';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="MemoryCard">
+class MemoryCard extends Component {
+	constructor() {
+		super();
+		this.state = {isFlipped: false};
 
-                <img src={"https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"}/>
+	}
+	clickHandler() {
+		this.setState({
+			isFlipped: !this.state.isFlipped
+		})
+	}
+    render() {
+        var memoryCardInnerClass = "MemoryCardInner";
+		if (this.state.isFlipped) {
+		    memoryCardInnerClass += " flipped";
+		}
+        return (
+            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+                <div className={memoryCardInnerClass}>
+                    <div className="MemoryCardBack">
+                        <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" />
+                    </div>
+                    <div className="MemoryCardFront">
+                        ∆
+                    </div>
+                </div>
             </div>
 
         );
     }
 }
 
-export default App;
+export default MemoryCard;
